f95009d1b2ccc28070d14e50f26de20c6aab08fe
ialoig <ialoig@protonmail.com>
2018-06-04 11:26:46 +0200

    Added chart amount_by_company & fixed amount_by_month

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,7 +1,40 @@
 class ChartsController < ApplicationController
 
 	def bills_amount_by_year
-		bills = Bill.group(:period).where('amount > 0').sum(:amount)
-		render json: [{name: 'Period', data: bills}]
-	end
+		bills = Bill.group(:period).where('amount > 0')
+
+		result = {}
+		bills.each do |b|
+			month = b.period.strftime("%B")
+			logger.debug "period = #{month}" 
+    		logger.debug "amount = #{b.amount}"
+    		amount = b.amount
+    		if result[month] == nil
+    			result[month] = amount
+    		else
+    			result[month] += amount
+    		end
+    		logger.debug "result = #{result}"
+  		end
+  		render json: [{name: 'Total (€)', data: result}]
+  	end
+
+  	def bills_amount_by_company
+		bills = Bill.group(:period).where('amount > 0')
+
+		result = {}
+		bills.each do |b|
+			month = b.period.strftime("%B")
+			logger.debug "period = #{month}" 
+    		logger.debug "company = #{b.company.name}"
+    		amount = b.amount
+    		if result[b.company.name] == nil
+    			result[b.company.name] = amount
+    		else
+    			result[b.company.name] += amount
+    		end
+    		logger.debug "result = #{result}"
+  		end
+  		render json: [{name: 'Total (€)', data: result}]
+  	end
 end
--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -2,18 +2,36 @@ module StatisticsHelper
   
   def bills_amount_by_year
     column_chart bills_amount_by_year_charts_path, height: '300px', library: {
-      title: {text: 'Amount by month', x: -20},
+      title: {text: 'Amount by Month', x: -20},
       yAxis: {
        title: {
-         text: 'Amount'
+         text: 'Amount (€)'
        }
      },
       xAxis: {
         title: {
           text: 'Month'
-        }
+        },
+        crosshair: true
       }
     }
  end
+
+  def bills_amount_by_company
+    column_chart bills_amount_by_company_charts_path, height: '300px', library: {
+      title: {text: 'Amount by Company', x: -20},
+      yAxis: {
+       title: {
+         text: 'Amount (€)'
+       }
+     },
+      xAxis: {
+        title: {
+          text: 'Company'
+        },
+        crosshair: true
+      }
+    }
+  end
 
 end
