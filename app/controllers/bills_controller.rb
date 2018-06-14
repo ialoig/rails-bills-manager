d0ddab20414ba702868aa07a8b35c669ee0bc8ab
ialoig <ialoig@protonmail.com>
2018-06-14 13:13:40 +0200

    Changed forms style & Added new Dashboard panel

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,29 +1,21 @@
 class BillsController < ApplicationController
   before_action :set_bill, only: [:show, :edit, :update, :destroy]
 
-  # GET /bills
-  # GET /bills.json
   def index
     @bills = Bill.all
   end
 
-  # GET /bills/1
-  # GET /bills/1.json
   def show
   end
 
-  # GET /bills/new
   def new
     @bill = Bill.new
     @company = @bill.build_company
   end
 
-  # GET /bills/1/edit
   def edit
   end
 
-  # POST /bills
-  # POST /bills.json
   def create
     @bill = Bill.new(bill_params)
 
@@ -37,8 +29,6 @@ class BillsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /bills/1
-  # PATCH/PUT /bills/1.json
   def update
     respond_to do |format|
       if @bill.update(bill_params)
@@ -50,8 +40,6 @@ class BillsController < ApplicationController
     end
   end
 
-  # DELETE /bills/1
-  # DELETE /bills/1.json
   def destroy
     @bill.destroy
     respond_to do |format|
