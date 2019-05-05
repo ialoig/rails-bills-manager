97bd257583b39372933ca52675d092901f3cd70a
Giovanni <giovannilai86@gmail.com>
2019-05-05 20:48:43 +0200

    Added authenticate_user before action

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,6 +1,6 @@
 class BillsController < ApplicationController
   before_action :set_bill, only: [:show, :edit, :update, :destroy]
-  
+  before_action :authenticate_user!
 
   def index
     @bills = Bill.all
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 
 	def index
+		if not signed_in?.blank?
+			redirect_to home_path
+		end
 	end
 
 end
